85ca51adac64a3a527efde198a1236dae1529801
a7med3365 <a7med3365@gmail.com>
2023-10-02 19:55:53 +0300

    added animaion and modified the footer

--- src/components/TrendCard.js
+++ src/components/TrendCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function TrendCard({ BgImg }) {
   return (
-    <div className="rounded-[5rem] overflow-hidden">
+    <div className="rounded-[5rem] overflow-hidden transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
       <img src={BgImg} alt="trending now" />
     </div>
   );
