8b6c81d29f03a9771f515dc7cc07a9bdfe05cbfd
a7med3365 <a7med3365@gmail.com>
2023-10-08 16:37:36 +0300

    finished the search component, added the preview images functionality

--- src/components/DetailsPreview.js
+++ src/components/DetailsPreview.js
@@ -1,20 +1,43 @@
 import React from 'react';
+import { useState } from 'react';
 
-export default function DetailsPreview({ images }) {
+export default function DetailsPreview() {
+  const [selected, setSelected] = useState(0);
+  let images = [
+    'https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg',
+    'https://app.requestly.io/delay/1000/https://nextui-docs-v2.vercel.app/images/fruit-4.jpeg',
+    'https://s3-alpha-sig.figma.com/img/69c4/6289/d851020acab2051ccbf43d558bf40678?Expires=1697414400&Signature=F2Lfn1Kn-Fj-Wymz~-6E~103dKRK8oof13xEhAhq4IWoQxX7ieen1ZcvAOT5P6b3n-bOcqMs4-lgSctjB-s2G~R~kdhE1SfHmGyjiqwvanY95eBScCyVM5Ekh~bsHYjmJltqgun3XvutCVWjji2EjreKFspRGTURAsEQPdQy9j3B8LQz2mAVk2ZxD8oo-R4Qc8TRRf2bF6frFTHgnjpd48Y8GiFayr8mpDK~JF6eqBssnXqsI62fsACObts6n5sjT00RLwRQTe~uEHwAH21dYA9geD6fxYKhfoY4oeehpeNQA3~iD5wxPSNkbiKVQZzpqZjd7Baf-8RC05Y2ET--1g__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+    'https://s3-alpha-sig.figma.com/img/956d/d96f/862bc77223ba455cf9e72786d7732c82?Expires=1697414400&Signature=fZGy8PfXBv3fXRW9Y3e6Yyu5htnWRZpy7wpXlg1OV3~Uri1LBWW6o1m25F0jnsn2UX9WRdrO1-fTtDXikA~24XnLLDokRm2IV9LLNFVUvMF4Y6dTzJeEQ3zkbtyG6SFevKD5sU9IhKGj40E6DSuS1jF5tAOZvC3G88405Jz90~ibw7FGH6iUW9V6MurRUT4SZNYI3oQ-OiryUWpKhtxv1gYgw3AhIpBKJxE6pBKgb-rOzW6bwwMMJy8029x5Mfo1hSJqrGPPzxQxEmI-Fmg3zIMUyFYzprF5KKiwstgU2WXqge4IMe-qFpDMfX7V69oe2J7JFSGO4BKbR8-Hdvj8Ww__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+    'https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg',
+    'https://app.requestly.io/delay/1000/https://nextui-docs-v2.vercel.app/images/fruit-4.jpeg',
+    'https://s3-alpha-sig.figma.com/img/69c4/6289/d851020acab2051ccbf43d558bf40678?Expires=1697414400&Signature=F2Lfn1Kn-Fj-Wymz~-6E~103dKRK8oof13xEhAhq4IWoQxX7ieen1ZcvAOT5P6b3n-bOcqMs4-lgSctjB-s2G~R~kdhE1SfHmGyjiqwvanY95eBScCyVM5Ekh~bsHYjmJltqgun3XvutCVWjji2EjreKFspRGTURAsEQPdQy9j3B8LQz2mAVk2ZxD8oo-R4Qc8TRRf2bF6frFTHgnjpd48Y8GiFayr8mpDK~JF6eqBssnXqsI62fsACObts6n5sjT00RLwRQTe~uEHwAH21dYA9geD6fxYKhfoY4oeehpeNQA3~iD5wxPSNkbiKVQZzpqZjd7Baf-8RC05Y2ET--1g__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+    'https://s3-alpha-sig.figma.com/img/956d/d96f/862bc77223ba455cf9e72786d7732c82?Expires=1697414400&Signature=fZGy8PfXBv3fXRW9Y3e6Yyu5htnWRZpy7wpXlg1OV3~Uri1LBWW6o1m25F0jnsn2UX9WRdrO1-fTtDXikA~24XnLLDokRm2IV9LLNFVUvMF4Y6dTzJeEQ3zkbtyG6SFevKD5sU9IhKGj40E6DSuS1jF5tAOZvC3G88405Jz90~ibw7FGH6iUW9V6MurRUT4SZNYI3oQ-OiryUWpKhtxv1gYgw3AhIpBKJxE6pBKgb-rOzW6bwwMMJy8029x5Mfo1hSJqrGPPzxQxEmI-Fmg3zIMUyFYzprF5KKiwstgU2WXqge4IMe-qFpDMfX7V69oe2J7JFSGO4BKbR8-Hdvj8Ww__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+  ];
   return (
     <div className="w-full relative flex flex-col gap-[42px]">
       <img
         className="rounded-[54px] w-[1000px]"
         alt="previewed"
-        src="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
+        src={images[selected]}
       />
       <div class="flex flex-col bg-white w-full relative">
         <div class="flex overflow-x-scroll pb-10 hide-scroll-bar">
           <div class="flex flex-nowrap gap-[20px]">
-            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((s) => {
+            {images.map((image, i) => {
               return (
-                <div key={s} class="inline-block">
-                  <div class="w-[158px] h-[118px] max-w-xs overflow-hidden rounded-[25px] shadow-md bg-gray1 hover:shadow-xl transition-shadow duration-300 ease-in-out"></div>
+                <div key={i} class="inline-block">
+                  <div
+                    class="w-[158px] h-[118px] max-w-xs overflow-hidden rounded-[25px] shadow-md bg-gray1 hover:shadow-xl transition-shadow duration-300 ease-in-out"
+                    onClick={() => {
+                      setSelected(i);
+                    }}
+                  >
+                    <img
+                      alt={i}
+                      src={image}
+                      className="object-cover h-[118px]"
+                    />
+                  </div>
                 </div>
               );
             })}
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,110 @@
 import React from 'react';
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  Button,
+} from '@nextui-org/react';
 
 export default function Search() {
   return (
-    <div className="w-[793px] h-[172px] bg-gray1 text-center text-4xl rounded-[12px]">
-      Search Component
+    <div className="w-[793px] h-[172px] flex flex-col justify-between">
+      <input
+        type="search"
+        label="Search"
+        placeholder="Search"
+        className="border-[1px] border-[#8B8B8B] rounded-[12px] h-[89px] text-center text-[#4B4B4B] font-medium text-[32px] leading-[39px] placeholder:italic placeholder:text-[#C0C0C0] focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 transition ease-in-out delay-150 shadow-lg focus:shadow-2xl duration-300"
+      />
+      <div className="flex justify-between">
+        <Dropdown>
+          <DropdownTrigger>
+            <Button
+              variant="bordered"
+              className="border-[1px] border-[#8B8B8B] rounded-[12px] h-[59px] flex gap-[44px] p-[10px] w-[207px]"
+            >
+              <p className="text-[#8B8B8B] text-[32px] leading-[39px] font-medium">
+                Genre
+              </p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="11"
+                viewBox="0 0 18 11"
+                fill="none"
+              >
+                <path
+                  d="M8.75665 10.0067L0.707107 1.95711C0.077142 1.32714 0.523309 0.25 1.41421 0.25H16.7467C17.6137 0.25 18.0699 1.27799 17.4882 1.92091L10.2053 9.97046C9.8216 10.3945 9.16105 10.411 8.75665 10.0067Z"
+                  fill="#8B8B8B"
+                />
+              </svg>
+            </Button>
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Static Actions">
+            <DropdownItem key="new">Option 1</DropdownItem>
+            <DropdownItem key="copy">Option 2</DropdownItem>
+            <DropdownItem key="edit">Option 3</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+        <Dropdown>
+          <DropdownTrigger>
+            <Button
+              variant="bordered"
+              className="border-[1px] border-[#8B8B8B] rounded-[12px] h-[59px] flex gap-[44px] p-[10px] w-[265px]"
+            >
+              <p className="text-[#8B8B8B] text-[32px] leading-[39px] font-medium">
+                Film Type
+              </p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="11"
+                viewBox="0 0 18 11"
+                fill="none"
+              >
+                <path
+                  d="M8.75665 10.0067L0.707107 1.95711C0.077142 1.32714 0.523309 0.25 1.41421 0.25H16.7467C17.6137 0.25 18.0699 1.27799 17.4882 1.92091L10.2053 9.97046C9.8216 10.3945 9.16105 10.411 8.75665 10.0067Z"
+                  fill="#8B8B8B"
+                />
+              </svg>
+            </Button>
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Static Actions" className="w-[250px]">
+            <DropdownItem key="new">Option 1</DropdownItem>
+            <DropdownItem key="copy">Option 2</DropdownItem>
+            <DropdownItem key="edit">Option 3</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+        <Dropdown>
+          <DropdownTrigger>
+            <Button
+              variant="bordered"
+              className="border-[1px] border-[#8B8B8B] rounded-[12px] h-[59px] flex gap-[44px] p-[10px] w-[275px]"
+            >
+              <p className="text-[#8B8B8B] text-[32px] leading-[39px] font-medium">
+                Language
+              </p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="11"
+                viewBox="0 0 18 11"
+                fill="none"
+              >
+                <path
+                  d="M8.75665 10.0067L0.707107 1.95711C0.077142 1.32714 0.523309 0.25 1.41421 0.25H16.7467C17.6137 0.25 18.0699 1.27799 17.4882 1.92091L10.2053 9.97046C9.8216 10.3945 9.16105 10.411 8.75665 10.0067Z"
+                  fill="#8B8B8B"
+                />
+              </svg>
+            </Button>
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Static Actions" className="w-[260px]">
+            <DropdownItem key="new">Option 1</DropdownItem>
+            <DropdownItem key="copy">Option 2</DropdownItem>
+            <DropdownItem key="edit">Option 3</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </div>
     </div>
   );
 }
