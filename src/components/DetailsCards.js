58b4483b9db5df17ddcdb5b4d1483d40531dfdce
Ahmed Alhamed <a7med3365@gmail.com>
2023-10-12 15:29:16 +0300

    updated the prject details page

--- src/components/DetailsCards.js
+++ src/components/DetailsCards.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import GenericLabel1 from '../components/GenericLabel1';
-import { Button, useDisclosure } from '@nextui-org/react';
-import ProgressPar from '../components/ProgressPar';
-import Rewards from './Rewards';
+import React from "react";
+import GenericLabel1 from "../components/GenericLabel1";
+import { Button, useDisclosure } from "@nextui-org/react";
+import ProgressPar from "../components/ProgressPar";
+import Rewards from "./Rewards";
 
 export default function DetailsCards({ data }) {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
   return (
     <div>
       <div className="flex flex-col w-[405px] gap-[19px]">
-        <div className="h-[82px] flex justify-center gap-[29px] py-7 border-[1px] border-[#D9D9D9] rounded-[27px]">
+        {/* <div className="h-[82px] flex justify-center gap-[29px] py-7 border-[1px] border-[#D9D9D9] rounded-[27px]">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="28"
@@ -181,18 +181,79 @@ export default function DetailsCards({ data }) {
               />
             </svg>
           </div>
-        </div>
-        <div className="rounded-[27px] w-full h-[114px] border-[1px] border-[#D9D9D9]">
-          <div className="px-[38px] py-[10px] flex justify-between">
-            <div className="rounded-full w-[91px] h-[91px] overflow-hidden">
-              <img alt="profile pic" src="./details-profile-pic-example.png" />
+        </div> */}
+        <div className="rounded-[27px] w-full h-[164px] border-[1px] border-[#D9D9D9]">
+          <div className="px-[38px] py-[25px] flex justify-between">
+            <div className="rounded-full w-[116.5px] h-[116.5px] overflow-hidden">
+              <img
+                alt="profile pic"
+                src="./details-profile-pic-example.png"
+                className="w-[116.5px] h-[116.5px] object-cover"
+              />
             </div>
-            <div className="my-auto">
-              <div className="text-[#656565] text-[23.718px] font-bold">
-                {data.name}
+            <div className="my-auto flex flex-col gap-[10px]">
+              <div>
+                <div className="text-[#656565] text-[24px] font-bold">
+                  {data.name}
+                </div>
+                <div className="text-[#A5A5A5] text-[16px] font-medium">
+                  {data.role + " | " + data.location}
+                </div>
               </div>
-              <div className="text-[#A5A5A5] text-[16px] font-medium">
-                {data.role + ' | ' + data.location}
+              <div className="flex justify-between">
+                <Button className="w-[88.2px] h-[39px] bg-orange text-white text-[14px] font-medium">
+                  Follow
+                </Button>
+                <Button className="w-[88.2px] h-[39px] bg-white border-[1px] border-orange">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="18"
+                    height="17"
+                    viewBox="0 0 18 17"
+                    fill="none"
+                  >
+                    <g clip-path="url(#clip0_769_1933)">
+                      <path
+                        d="M7.69971 15.9727H10.041"
+                        stroke="#F15A2B"
+                        stroke-width="1.17067"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                      <path
+                        d="M13.553 7.19196C13.553 5.95003 13.0597 4.75898 12.1815 3.8808C11.3033 3.00263 10.1123 2.50928 8.87032 2.50928C7.6284 2.50928 6.43734 3.00263 5.55917 3.8808C4.681 4.75898 4.18765 5.95003 4.18765 7.19196V11.2893C4.18765 11.755 4.00263 12.2017 3.67332 12.531C3.34401 12.8603 2.89736 13.0453 2.43164 13.0453H15.309C14.8433 13.0453 14.3966 12.8603 14.0673 12.531C13.738 12.2017 13.553 11.755 13.553 11.2893V7.19196Z"
+                        stroke="#F15A2B"
+                        stroke-width="1.17067"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                      <path
+                        d="M1.26123 7.04003C1.26186 5.92572 1.5276 4.82755 2.03649 3.83622C2.54539 2.8449 3.28283 1.98888 4.1879 1.33887"
+                        stroke="#F15A2B"
+                        stroke-width="1.17067"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                      <path
+                        d="M16.4797 7.04003C16.4791 5.92572 16.2133 4.82755 15.7044 3.83622C15.1955 2.8449 14.458 1.98888 13.553 1.33887"
+                        stroke="#F15A2B"
+                        stroke-width="1.17067"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                    </g>
+                    <defs>
+                      <clipPath id="clip0_769_1933">
+                        <rect
+                          width="16.3894"
+                          height="16.3894"
+                          fill="white"
+                          transform="translate(0.676758 0.461914)"
+                        />
+                      </clipPath>
+                    </defs>
+                  </svg>
+                </Button>
               </div>
             </div>
           </div>
@@ -201,8 +262,8 @@ export default function DetailsCards({ data }) {
         <div className="rounded-[27px] w-full h-[241px] border-[1px] border-[#D9D9D9]">
           <div className="px-[38px] py-[24px]">
             <div className="flex gap-[75px] mb-[20px]">
-              <GenericLabel1 label={'Film Type'} value={data.filmType} />
-              <GenericLabel1 label={'Genre'} value={data.genre} />
+              <GenericLabel1 label={"Film Type"} value={data.filmType} />
+              <GenericLabel1 label={"Genre"} value={data.genre} />
             </div>
             <div className="text-[#A5A5A5] text-[18px] leading-[22px] font-medium mb-[10px]">
               Donation Progress
@@ -213,7 +274,7 @@ export default function DetailsCards({ data }) {
                   Raised
                 </p>
                 <p className="text-[#656565] text-[16px] leading-[19px] font-bold">
-                  {'BD ' + data.fundRaised}
+                  {"BD " + data.fundRaised}
                 </p>
               </div>
               <div>
@@ -221,7 +282,7 @@ export default function DetailsCards({ data }) {
                   Goal
                 </p>
                 <p className="text-[#656565] text-[16px] leading-[19px] font-bold">
-                  {'BD ' + data.fundGoal}
+                  {"BD " + data.fundGoal}
                 </p>
               </div>
             </div>
@@ -234,10 +295,10 @@ export default function DetailsCards({ data }) {
         <div className="rounded-[27px] w-full h-[433px] border-[1px] border-[#D9D9D9]">
           <div className="px-[38px] py-[24px]">
             <div className="grid grid-cols-2 gap-x-[40px] gap-y-[40px] mb-[39px]">
-              <GenericLabel1 label={'Fund Purpose'} value={data.fundPurpose} />
-              <GenericLabel1 label={'Deadline'} value={data.Deadline} />
-              <GenericLabel1 label={'Fund Type'} value={data.type} />
-              <GenericLabel1 label={'Rewards'} value={data.rewards} />
+              <GenericLabel1 label={"Fund Purpose"} value={data.fundPurpose} />
+              <GenericLabel1 label={"Deadline"} value={data.Deadline} />
+              <GenericLabel1 label={"Fund Type"} value={data.type} />
+              <GenericLabel1 label={"Rewards"} value={data.rewards} />
             </div>
             <div className="mb-[27px]">
               <div className="text-[18px] text-[#656565] leading-[22px] font-medium flex justify-between">
--- src/components/DetailsTaps.js
+++ src/components/DetailsTaps.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Tabs, Tab } from '@nextui-org/react';
+import React from "react";
+import { Tabs, Tab } from "@nextui-org/react";
 
-import DetailsStoryExample from './DetailsStoryExample';
+import DetailsStoryExample from "./DetailsStoryExample";
 
 export default function DetailsTaps() {
   return (
@@ -10,11 +10,11 @@ export default function DetailsTaps() {
         variant="underlined"
         aria-label="Project Details Tabs"
         classNames={{
-          tabList: 'gap-[41px] w-full relative rounded-none p-0 ',
-          cursor: 'w-full bg-orange h-[7px] rounded-full',
-          tab: 'max-w-fit px-0 h-12',
+          tabList: "gap-[41px] w-full relative rounded-none p-0 ",
+          cursor: "w-full bg-orange h-[7px] rounded-full",
+          tab: "max-w-fit px-0 h-12",
           tabContent:
-            'group-data-[selected=true]:text-orange text-[24px] leading-[29px] text-[#656565] mb-[21px]',
+            "group-data-[selected=true]:text-orange text-[24px] leading-[29px] text-[#656565] mb-[21px]",
         }}
       >
         <Tab key="Story" title="Story">
@@ -26,9 +26,10 @@ export default function DetailsTaps() {
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat.
         </Tab>
+        <Tab key="Script" title="Script" />
         <Tab key="Budget" title="Budget" />
         <Tab key="Updates" title="Updates"></Tab>
-        <Tab key="Comments" title="Comments"></Tab>
+        <Tab key="Discussion" title="Discussion"></Tab>
       </Tabs>
     </div>
   );
