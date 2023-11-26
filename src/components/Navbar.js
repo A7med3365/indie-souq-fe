ca8731aad3fea5fdb9438072577df160219501d0
Ahmed Alhamed <aalhamed@ngnintl.com>
2023-11-26 19:09:21 +0300

    updated the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,86 +7,119 @@ import {
   Link,
   Button,
   Spinner,
+  useDisclosure,
+  Input,
 } from '@nextui-org/react';
 import NavbarAvatar from './NavbarAvatar';
+import MyModal from './MyModal';
 
 export default function Navbar({ userId, isAuth, logout }) {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
   return (
-    <Nav maxWidth="2xl" className="px-5 2xl:px-0" isBlurred>
-      <NavbarContent justify="start">
-        <Link href="/">
-          <NavbarBrand>
-            <img src="./logo.png" alt="logo" />
-          </NavbarBrand>
-        </Link>
-      </NavbarContent>
-      <NavbarContent
-        className="hidden sm:flex gap-3 lg:gap-10"
-        justify="center"
-      >
-        <NavbarItem>
-          <Link
-            className="text-[#D8D8D8] text-base font-semibold p-2.5 hover:text-gray1"
-            href="/films"
-          >
-            Explore films
+    <>
+      <Nav maxWidth="2xl" className="px-5 2xl:px-0" isBlurred>
+        <NavbarContent justify="start">
+          <Link href="/">
+            <NavbarBrand>
+              <img src="./logo.png" alt="logo" />
+            </NavbarBrand>
           </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link
-            className="text-[#D8D8D8] text-base font-semibold p-2.5 hover:text-gray1"
-            href="/creators"
-          >
-            Connect with creators
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link
-            className="text-[#D8D8D8] text-base font-semibold p-2.5 hover:text-gray1"
-            href="/"
-          >
-            Learn about IndieSouq
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
-      <NavbarContent justify="end" className="flex gap-[8px]">
-        <NavbarItem>
+        </NavbarContent>
+        <NavbarContent
+          className="hidden sm:flex gap-3 lg:gap-10"
+          justify="center"
+        >
+          <NavbarItem>
+            <Link
+              className="text-[#D8D8D8] text-base font-semibold p-2.5 hover:text-gray1"
+              href="/films"
+            >
+              Explore films
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link
+              className="text-[#D8D8D8] text-base font-semibold p-2.5 hover:text-gray1"
+              href="/creators"
+            >
+              Connect with creators
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link
+              className="text-[#D8D8D8] text-base font-semibold p-2.5 hover:text-gray1"
+              href="/"
+            >
+              Learn about IndieSouq
+            </Link>
+          </NavbarItem>
+        </NavbarContent>
+        <NavbarContent justify="end" className="flex gap-[8px]">
+          <NavbarItem>
+            <Button
+              as={Link}
+              className="bg-orange w-[163px] h-[44.13px] p-[12.56px] rounded-xl"
+              href="/"
+              variant="flat"
+            >
+              <p className="text-white text-base font-semibold px-2">
+                Support project
+              </p>
+            </Button>
+          </NavbarItem>
+          <NavbarItem>
+            <Button
+              // as={Link}
+              className="border border-orange bg-[rgb(0,0,0,0)] w-[163px] h-[44.13px] p-[12.56px] rounded-xl"
+              // href="/"
+              variant="flat"
+              onClick={onOpen}
+            >
+              <p className="text-orange text-base font-semibold px-2">
+                Submit project
+              </p>
+            </Button>
+          </NavbarItem>
+          <NavbarItem className="hidden lg:flex">
+            {isAuth ? (
+              <NavbarAvatar userId={userId} logout={logout} />
+            ) : (
+              <Link
+                className="text-gray1 text-base font-semibold p-[10px]"
+                href="/signin"
+              >
+                login
+              </Link>
+            )}
+          </NavbarItem>
+        </NavbarContent>
+      </Nav>
+      <MyModal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <div className=" w-2/4 h-fit p-9 bg-white rounded-2xl flex flex-col gap-5">
+          <p className=" text-orange text-lg">Your project Title:</p>
+          <Input
+            // isDisabled
+            variant="bordered"
+            size="lg"
+            type="text"
+            // label="Project Title"
+            // labelPlacement=""
+            placeholder="Enter Title"
+            classNames={{
+              inputWrapper: 'text-gray2',
+            }}
+          />
           <Button
-            as={Link}
-            className="bg-orange w-[163px] h-[44.13px] p-[12.56px] rounded-xl"
-            href="/"
+            className="border border-orange bg-[rgb(0,0,0,0)] w-[163px] h-[44.13px] p-[12.56px] rounded-xl mx-auto mt-4"
             variant="flat"
-          >
-            <p className="text-white text-base font-semibold px-2">
-              Support project
-            </p>
-          </Button>
-        </NavbarItem>
-        <NavbarItem>
-          <Button
-            as={Link}
-            className="border border-orange bg-[rgb(0,0,0,0)] w-[163px] h-[44.13px] p-[12.56px] rounded-xl"
-            href="/"
-            variant="flat"
+            onClick={onOpen}
           >
             <p className="text-orange text-base font-semibold px-2">
-              Submit project
+              Create project
             </p>
           </Button>
-        </NavbarItem>
-        <NavbarItem className="hidden lg:flex">
-          {isAuth ? (
-            <NavbarAvatar userId={userId} logout={logout} />
-          ) : (
-            <Link
-              className="text-gray1 text-base font-semibold p-[10px]"
-              href="/signin"
-            >
-              login
-            </Link>
-          )}
-        </NavbarItem>
-      </NavbarContent>
-    </Nav>
+        </div>
+      </MyModal>
+    </>
   );
 }
