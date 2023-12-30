3212f63c4678d8f479b98f48cc62a54ab5607582
a7med3365 <a7med3365@gmail.com>
2023-12-30 12:13:45 +0300

    populated story and budget taps, disabled the rest fo now

--- src/components/DetailsTaps.js
+++ src/components/DetailsTaps.js
@@ -1,15 +1,16 @@
-import React from "react";
-import { Tabs, Tab } from "@nextui-org/react";
+import React from 'react';
+import { Tabs, Tab } from '@nextui-org/react';
+import { PieChart } from 'react-minimal-pie-chart';
 
-import DetailsStoryExample from "./DetailsStoryExample";
+// import DetailsStoryExample from "./DetailsStoryExample";
 
-export default function DetailsTaps() {
+export default function DetailsTaps({ story, budget }) {
   return (
     <div>
       <Tabs
         variant="underlined"
         aria-label="Project Details Tabs"
-        disabledKeys={['Cast','Script','Updates','Discussion']}
+        disabledKeys={['Cast', 'Script', 'Updates', 'Discussion']}
         classNames={{
           tabList: 'gap-[41px] w-full relative rounded-none p-0 ',
           cursor: 'w-full bg-orange h-[7px] rounded-full',
@@ -19,16 +20,66 @@ export default function DetailsTaps() {
         }}
       >
         <Tab key="Story" title="Story">
-          <DetailsStoryExample />
+          {/* <DetailsStoryExample /> */}
+          <div className="text-[24px] leading-[29px] text-[#656565] font-normal mt-5">
+            {story ? story : 'No Story'}
+          </div>
         </Tab>
-        <Tab key="Cast" title="Cast">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </Tab>
+        <Tab key="Cast" title="Cast"></Tab>
         <Tab key="Script" title="Script" />
-        <Tab key="Budget" title="Budget" />
+        <Tab key="Budget" title="Budget">
+          {budget ? (
+            <>
+              <div className="flex gap-2">
+                {budget.map(({ name, color }) => {
+                  const style = `inline-flex items-center rounded-xl bg-[${color}] px-4 py-2 text-lg font-medium text-[${color}] ring-4 ring-inset ring-[${color}]/10`;
+                  return <span className={style}>{name}</span>;
+                })}
+              </div>
+              <PieChart
+                style={{
+                  fontFamily:
+                    '"Nunito Sans", -apple-system, Helvetica, Arial, sans-serif',
+                  fontSize: '8px',
+                }}
+                labelPosition={112}
+                labelStyle={(index) => ({
+                  fill: budget[index].color,
+                  fontSize: '5px',
+                  fontFamily: 'sans-serif',
+                })}
+                // segmentsShift={(index) => (index === selected ? 5 : 1)}
+                animate
+                radius={30}
+                // lineWidth={90}
+                data={budget.map(
+                  ({ name: title, percentage: value, color }) => ({
+                    title,
+                    value,
+                    color,
+                  })
+                )}
+                totalValue={100}
+                label={({ dataEntry }) =>
+                  Math.round(dataEntry.percentage) + '%'
+                }
+                segmentsStyle={{ transition: 'stroke .3s', cursor: 'pointer' }}
+                // onClick={(_, index) => {
+                //   setSelected(index === selected ? 0 : index);
+                // }}
+                // onMouseOver={(_, index) => {
+                //   // @ts-ignore
+                //   setHovered(index);
+                // }}
+                // onMouseOut={() => {
+                //   setHovered(undefined);
+                // }}
+              />
+            </>
+          ) : (
+            'budget not finished yet'
+          )}
+        </Tab>
         <Tab key="Updates" title="Updates"></Tab>
         <Tab key="Discussion" title="Discussion"></Tab>
       </Tabs>
