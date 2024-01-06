3844794fe7f64902f23999a04293dbb0151b3cdd
a7med3365 <a7med3365@gmail.com>
2024-01-06 21:37:48 +0300

    testing - Add charge creation and retrieval functionality

--- src/pages/Tests.js
+++ src/pages/Tests.js
@@ -1,13 +1,28 @@
+// ts-check
+import axios from 'axios';
 import React from 'react';
-
-import Welcome from '../components/signup/Welcome';
+import { useLocation } from 'react-router-dom';
 
 export default function Test() {
-  
+  const location = useLocation();
+  const query = new URLSearchParams(location.search);
+
+  if (query.get('tap_id')) {
+    axios
+      .get(`/api/charge/${query.get('tap_id')}`)
+      .then((response) => console.log(response.data))
+      .catch((error) => console.error(error));
+  }
+
+  const createCharge = async () => {
+
+    axios.post('/api/charge', { amount: 2500, redirect: 'http://localhost:3000/t'  })
+      .then((response) => window.location.replace(response.data.url))
+      .catch((error) => console.error(error));
+  };
   return (
-    <>
-      <Welcome isSignup/>
-    </>
+    <div>
+      <button onClick={createCharge}>Click me</button>
+    </div>
   );
 }
-
